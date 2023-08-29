66412214c28c33ea2f299d39e8cbde4118c9821e
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-29 19:37:55 +0300

    added a solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
+        int number = 546;
 
+        int lastDigit = number % 10; //write your code here
+
+        System.out.println("The last digit of the number " + number + " is " + lastDigit);
     }
 }
